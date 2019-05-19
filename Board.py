c723bd7a9b5c058d423406684576e9f69a4326e2
hang2loose <hang2loose@hotmail.de>
2019-05-19 17:47:48 +0200

    added set ship

--- Board.py
+++ Board.py
@@ -12,3 +12,27 @@ class Board:
 
     def draw_board(self):
         pass
+
+
+class Ship:
+    def __init__(self, size: int, pos: int, horizontal: bool, board: tuple):
+        self.occupied_fields = None
+        self.size = size
+        self.board = board
+        self.horizontal = horizontal
+        self.pos = pos
+
+    def set_ship(self, size: int):
+        for i in range(size - 1):
+            if self.horizontal:
+                if self.board[self.pos + 1][self.pos].occupied:
+                    self.occupied_fields = None
+                    return
+                self.occupied_fields.append(self.board[self.pos + 1][self.pos])
+            else:
+                if self.board[self.pos + 1][self.pos].occupied:
+                    self.occupied_fields = None
+                    return
+                self.occupied_fields.append(self.board[self.pos][self.pos + 1])
+        for field in self.occupied_fields:
+            field.occupied = True
