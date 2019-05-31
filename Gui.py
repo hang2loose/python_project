3a9decc0223bbca281089bbb3ad39663768a1e47
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-05-31 14:09:34 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -1,21 +1,27 @@
 import random
 from tkinter import *
 
+
+class Battleships:
+    def __init__(self):
+        self.water1 = PhotoImage(file="water_01.gif")
+        self.water2 = PhotoImage(file="water_02.gif")
+        self.water3 = PhotoImage(file="water_03.gif")
+
+        self.fire = PhotoImage(file="fire.gif")
+
+        self.water = [self.water1, self.water2, self.water3]
+
+        self.button = Button(root, image=random.choice(self.water), height=32, width=32,
+                             command=lambda: root.config(image=self.fire)).grid(row=1)
+
+
 root = Tk()
-
-water1 = PhotoImage(file="water_01.gif")
-water2 = PhotoImage(file="water_02.gif")
-water3 = PhotoImage(file="water_03.gif")
-
-water = [water1, water2, water3]
-fire = PhotoImage(file="fire.gif")
 
 Label(root, text="Header").grid(row=0, columnspan=10)
 
 Label(root, text="Content").grid(row=1)
-
-Button(root, image=random.choice(water), height=32, width=32).grid(row=1).bind("<Button-1>")
-
+app = Battleships()
 
 Label(root, text="Footer").grid(row=11, columnspan=10)
 
