8feac8f7000867c036b4df76c569df74cd72d7b2
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-06-10 22:06:50 +0200

    Gui Update

--- Client/Gui.py
+++ Client/Gui.py
@@ -1,43 +1,62 @@
 import random
 import socket
-from BattleShips import Board
+
 from appJar import gui
 
-class Game:
+
+class GUI:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def __init__(self, appJar):
         self.sock.connect(("127.0.0.1", 8080))
-        self.msg = ""
 
-        self.board = Board(10).board
+        self.state = "game"
 
         self.gui = appJar
         self.gui.setTitle("Battleships")
         self.gui.setLocation("CENTER")
 
-        self.gui.setImageLocation("./images")
+        self.gui.setImageLocation("./Client/images")
         self.water = ["dark.gif", "medium.gif", "light.gif"]
 
-        self.gui.thread(self.get)
+        self.gui.thread(self.receive)
 
-    def get(self):
+    def receive(self):
         while True:
             data = self.sock.recv(1024)
             if not data:
                 break
             self.gui.setImage(str(data, 'utf-8'), "fire.gif")
 
-    def set(self, title):
+    def send(self, title):
         self.sock.send(bytes(title, 'utf-8'))
 
+    def set(self, pos):
+        pass
+
+    def board(self):
+        return 10
+
+    def hit(self, pos):
+        pass
+
+    def miss(self, pos):
+        pass
+
+    def draw_framework(self):
+        self.gui.startLabelFrame("State", 0)
+        self.gui.addLabel("l1", "Label 1")
+        self.gui.stopLabelFrame()
+
+        self.gui.startLabelFrame("Logic", 2)
+        self.gui.addLabel("l2", "Label 2")
+        self.gui.stopLabelFrame()
+
     def draw_board(self):
+        self.gui.startLabelFrame("Game Board", 1)
 
-        self.gui.startLabelFrame("Game Board")
-
-        for row in range(len(self.board)):
-            for column in range(len(self.board)):
-                self.gui.setSticky("nesw")
+        for row in range(self.board()):
+            for column in range(self.board()):
                 self.gui.addImage("{},{}".format(column, row), random.choice(self.water), column, row)
                 self.gui.setImageSubmitFunction("{},{}".format(column, row), self.set)
 
@@ -45,5 +64,7 @@ class Game:
 
 
 app = gui()
-Game(app).draw_board()
+gui = GUI(app)
+gui.draw_framework()
+gui.draw_board()
 app.go()
---
+++ Client/Handler.py
@@ -0,0 +1,7 @@
+class Handler:
+    def __init__(self, dict):
+        self.dict = dict
+
+    def board(self, message):
+        pass
+
