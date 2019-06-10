e3cf34f6f881a67d720d2b44c010758a73890b5b
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-06-10 18:20:38 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -6,22 +6,32 @@ from appJar import gui
 class Game:
     def __init__(self, appJar):
         self.board = Board(10).board
+
         self.gui = appJar
         self.gui.setTitle("Battleships")
-        self.gui.setSize("800x600")
         self.gui.setLocation("CENTER")
+
         self.gui.setImageLocation("./images")
         self.water = ["dark.gif", "medium.gif", "light.gif"]
 
-    def drawBoard(self):
-        for entry in self.board:
-            print(entry)
-            self.gui.addImage(entry, random.choice(self.water))
+    def fire_at_pos(self, title):
+        self.gui.setImage(title, "fire.gif")
 
+    def draw_board(self):
+
+        self.gui.startLabelFrame("Game Board")
+
+        for row in range(len(self.board)):
+            for column in range(len(self.board)):
+                self.gui.setSticky("nesw")
+                self.gui.addImage(str(column) + ", " + str(row), random.choice(self.water), row, column)
+                self.gui.setImageSubmitFunction(str(column) + ", " + str(row), self.fire_at_pos)
+
+        self.gui.stopLabelFrame()
 
 
 app = gui()
 
-Game(app).drawBoard()
+Game(app).draw_board()
 
 app.go()
