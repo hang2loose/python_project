1cf16aaae2f041451ab550f87dbb3ed660584715
hang2loose <hang2loose@hotmail.de>
2019-06-10 16:55:23 +0200

    start

---
+++ BattleShips.py
@@ -0,0 +1,90 @@
+from enum import Enum
+
+class Field:
+    occupied = False
+    state = "empty"
+
+    def print_field(self):
+        if not self.occupied:
+            return "O"
+        return "X"
+
+
+class Board:
+    def __init__(self, size: int):
+        self.size = size
+        self.board = self.create_board()
+
+    def create_board(self):
+        return tuple([tuple([Field() for column in range(self.size)]) for row in range(self.size)])
+
+    def print_board(self):
+        for row in self.board:
+            for field in row:
+                print("{} ".format(field.print_field()), end='')
+            print()
+
+    def set_ship(self):
+        pass
+    
+class Ship_Type(Enum):
+    SMALL = 2
+    MEDIUM = 3
+    BIG = 4
+        
+class Ship:
+    
+    def __init__(self, ship_type: Ship_Type):
+        self.occupied_fields = []
+        self.size = ship_type.value
+
+    def is_ship_alive(self):
+        pass
+
+class Player:
+
+    def __init__(self):
+        pass
+
+    def still_has_ships(self):
+        pass
+
+
+class Ship1:
+    def __init__(self, size: int, board: Board):
+        self.occupied_fields = []
+        self.size = size
+        self.board = board
+        self.in_water = False
+
+    def set_ship(self, pos: tuple, horizontal: bool):
+        for i in range(self.size - 1):
+            if horizontal:
+                if self.board[pos[0] + 1][pos[1]].occupied:
+                    self.occupied_fields = []
+                    return
+                self.occupied_fields.append(self.board[pos[0] + 1][pos[1]])
+            else:
+                if self.board[pos[0] + 1][pos[1]].occupied:
+                    self.occupied_fields = []
+                    return
+                self.occupied_fields.append(self.board[pos[0]][pos[1] + 1])
+        for field in self.occupied_fields:
+            field.occupied = True
+        self.in_water = True
+        return self
+
+
+
+class Game:
+
+    def __init__(self):
+        self.board1 = Board(10)
+
+    def print_board(self):
+        self.board1.print_board()
+
+
+board = Board(10)
+board.print_board()
+ship1 = ship1
--- Board.py
+++ Board.py
@@ -2,20 +2,33 @@ class Field:
     occupied = False
     state = "empty"
 
+    def print_field(self):
+        if not self.occupied:
+            return "O"
+        return "X"
+
 
 class Board:
     def __init__(self, size: int):
         self.size = size
+        self.board = self.create_board()
 
     def create_board(self):
         return tuple([tuple([Field() for column in range(self.size)]) for row in range(self.size)])
 
+    def print_board(self):
+        for row in self.board:
+            for field in row:
+                print("{} ".format(field.print_field()))
+            print("\n")
+
 
 class Ship:
-    def __init__(self, size: int, board: tuple):
+    def __init__(self, size: int, board: Board):
         self.occupied_fields = []
         self.size = size
         self.board = board
+        self.in_water = False
 
     def set_ship(self, pos: tuple, horizontal: bool):
         for i in range(self.size - 1):
@@ -31,11 +44,12 @@ class Ship:
                 self.occupied_fields.append(self.board[pos[0]][pos[1] + 1])
         for field in self.occupied_fields:
             field.occupied = True
+        self.in_water = True
         return self
 
 
 class Player:
-    def __init__(self, board: tuple, ships: list):
+    def __init__(self, board: Board, ships: list):
         self.board = board
         self.ships = ships
 
@@ -45,6 +59,7 @@ class Player:
             y_pos = int(input("Ypos: "))
             pos = (x_pos, y_pos)
             ship.set_ship(pos, True)
+        self.board.print_board()
 
     def shoot(self):
         x_pos = int(input("Xpos: "))
@@ -65,19 +80,19 @@ class Game:
         board_a = Board(10).create_board()
         board_b = Board(10).create_board()
 
-        ships_A = [Ship(2, board_a), Ship(2, board_a), Ship(4, board_a)]
-        ships_B = [Ship(2, board_b), Ship(2, board_b), Ship(4, board_b)]
+        ships_a = [Ship(2, board_a), Ship(2, board_a), Ship(4, board_a)]
+        ships_b = [Ship(2, board_b), Ship(2, board_b), Ship(4, board_b)]
 
-        self.player_A = Player(board_a, ships_A)
-        self.player_B = Player(board_b, ships_B)
+        self.player_a = Player(board_a, ships_a)
+        self.player_b = Player(board_b, ships_b)
 
     def players_set_ship(self):
-        self.player_A.set_ships()
-        self.player_B.set_ships()
+        self.player_a.set_ships()
+        self.player_b.set_ships()
 
     def play(self):
         self.players_set_ship()
         player_a_turn = True
         while True:
-            player_a_turn if self.player_A.shoot() else self.player_B.shoot()
+            player_a_turn if self.player_a.shoot() else self.player_b.shoot()
             player_a_turn = not player_a_turn
