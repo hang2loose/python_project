37672b84b98faa13be21afbbc1193a3b34fd2ae7
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-06-10 20:39:58 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -1,11 +1,15 @@
 import random
-from BattleShips import Board, Field
-from BattleShipsEnums import *
+import socket
+from BattleShips import Board
 from appJar import gui
 
-
 class Game:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     def __init__(self, appJar):
+        self.sock.connect(("127.0.0.1", 8080))
+        self.msg = ""
+
         self.board = Board(10).board
 
         self.gui = appJar
@@ -15,9 +19,17 @@ class Game:
         self.gui.setImageLocation("./images")
         self.water = ["dark.gif", "medium.gif", "light.gif"]
 
-    def fire_at_pos(self, title):
-        self.gui.setImage(title, "fire.gif")
-        print(title)
+        self.gui.thread(self.get)
+
+    def get(self):
+        while True:
+            data = self.sock.recv(1024)
+            if not data:
+                break
+            self.gui.setImage(str(data, 'utf-8'), "fire.gif")
+
+    def set(self, title):
+        self.sock.send(bytes(title, 'utf-8'))
 
     def draw_board(self):
 
@@ -27,13 +39,11 @@ class Game:
             for column in range(len(self.board)):
                 self.gui.setSticky("nesw")
                 self.gui.addImage("{},{}".format(column, row), random.choice(self.water), column, row)
-                self.gui.setImageSubmitFunction("{},{}".format(column, row), self.fire_at_pos)
+                self.gui.setImageSubmitFunction("{},{}".format(column, row), self.set)
 
         self.gui.stopLabelFrame()
 
 
 app = gui()
-
 Game(app).draw_board()
-
 app.go()
