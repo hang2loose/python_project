9421b96a2e2a7b6e8bf848d967b70bc39afe365d
hang2loose <hang2loose@hotmail.de>
2019-06-30 17:10:58 +0200

    server registers players

--- GameServer.py
+++ GameServer.py
@@ -7,24 +7,26 @@ app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
+players_list = []
 battle_ships = Game()
 
 
 @sio.event
 def connect(sid, environ):
+    players_list.append(sid)
     print('connect ', sid)
 
 
 @sio.event
 def disconnect(sid):
+    players_list.remove(sid)
     print('disconnect ', sid)
 
 
 @sio.on('shoot_at')
 def handle_player_shot(sid, payload):
     print("pizza " + payload)
-    battle_ships.print_game_state()
-    sio.emit('miss', payload)
+    sio.emit('hit', payload, room=sid)
 
 
 if __name__ == '__main__':
