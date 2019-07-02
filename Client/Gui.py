c5b00b31531bef1bb375eda0e798fd3602c570be
SaveEnergy <46645793+SaveEnergy@users.noreply.github.com>
2019-07-02 21:15:32 +0200

    Fixed Path

--- Client/Gui.py
+++ Client/Gui.py
@@ -1,6 +1,18 @@
+import sys, os
 import random
 import socketio
 from appJar import gui
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.environ.get("_MEIPASS2",os.path.abspath("."))
+
+    return os.path.join(base_path, relative_path)
 
 
 class EventHandler:
@@ -118,7 +130,7 @@ class GUI:
 
         self.gui.setStretch("None")
         self.gui.setGuiPadding(100, 20)
-        self.gui.setImageLocation("./Client/images")
+        self.gui.setImageLocation(resource_path("images"))
 
     def canvas_board(self, board_name):
         self.gui.addCanvas(board_name)
